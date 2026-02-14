f27d14b29c148c9c1feb87fe711885b076514d67
andres777-b <omgandrex@gmail.com>
2026-02-13 21:15:36 -0500

    Commit final Resultado

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,12 @@
-let opcion=prompt("Ingrese la operacion que quiere realizar\n1. Suma\n2. Sumar\n3. Multiplicar\n4. Dividir");
+let opcion=prompt("Ingrese la operacion que quiere realizar\n1. Suma\n2. Restar\n3. Multiplicar\n4. Dividir");
 let num1=prompt("Ingrese el primer numero");
 let num2=prompt("Ingrese el segundo numero");
+if (opcion == "1"){
+    document.writeln("Resultado de la suma: ", num1+num2);
+} else if(opcion == "2"){
+    document.writeln("Resultado de la resta: ", num1-num2);
+} else if(opcion == "3"){
+    document.writeln("Resultado de la Multiplicacion: ", num1*num2);
+} else if(opcion == "4"){
+    document.writeln("Resultado de la Division: ", num1/num2);
+}
