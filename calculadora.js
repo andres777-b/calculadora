40d9a9ebd4328fcd845fc8b91311838ef35c070a
andres777-b <omgandrex@gmail.com>
2026-02-13 21:03:44 -0500

    Elige el segundo numero de la operacion

--- calculadora.js
+++ calculadora.js
@@ -1,2 +1,3 @@
 let opcion=prompt("Ingrese la operacion que quiere realizar\n1. Suma\n2. Sumar\n3. Multiplicar\n4. Dividir");
 let num1=prompt("Ingrese el primer numero");
+let num2=prompt("Ingrese el segundo numero");
